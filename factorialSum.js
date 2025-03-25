9d51477ad72db631ffdbce13c7bda1cc20351be4
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-03-25 09:44:00 +0300

    new var

--- factorialSum.js
+++ factorialSum.js
@@ -1,35 +1,31 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Функция для вычисления факториала
-function factorial(n) {
-    if (n === 0 || n === 1) return BigInt(1);
-    let result = BigInt(1);
-    for (let i = 2; i <= n; i++) {
-        result *= BigInt(i);
-    }
-    return result;
-}
-
-// Функция для подсчёта суммы цифр
-function sumOfDigits(num) {
-    return num.toString().split('').reduce((sum, digit) => sum + Number(digit), 0);
-}
-
-// Запрос числа у пользователя
-rl.question('Введите число: ', (input) => {
-    const n = parseInt(input);
-    if (isNaN(n) || n < 0) {
+const factorialSum = (n) => {
+    if (n < 0 || isNaN(n)) {
         console.log('Введите корректное неотрицательное число.');
-    } else {
-        const fact = factorial(n);
-        const sum = sumOfDigits(fact);
-        console.log(`Факториал: ${fact}`);
-        console.log(`Сумма цифр факториала: ${sum}`);
+        return;
     }
-    rl.close();
-});
+
+    // Функция для вычисления факториала
+    const factorial = (num) => {
+        let result = BigInt(1);
+        for (let i = 2; i <= num; i++) {
+            result *= BigInt(i);
+        }
+        return result;
+    };
+
+    // Функция для подсчёта суммы цифр
+    const sumOfDigits = (num) => {
+        return num.toString()
+            .split('')
+            .reduce((sum, digit) => sum + Number(digit), 0);
+    };
+
+    const fact = factorial(n);
+    const sum = sumOfDigits(fact);
+
+    console.log(`Факториал числа ${n}: ${fact}`);
+    console.log(`Сумма цифр факториала: ${sum}`);
+};
+
+const inputNumber = parseInt(process.argv[2], 10) || 10;
+factorialSum(inputNumber);
