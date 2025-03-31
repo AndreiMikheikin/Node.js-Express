a4175fe8d27bf341ef3c6a7664115b9921425718
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-03-31 15:30:25 +0300

    server ip

--- server/app.js
+++ server/app.js
@@ -18,5 +18,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://178.250.247.67/:${PORT}`);
 });
