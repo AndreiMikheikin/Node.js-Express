a260588065ffea640a4bf4f2e4116ebf2dd68c3b
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-04-29 13:36:17 +0300

    cT application/octet-stream adding

--- server/routes/tasks/extraTask1.js
+++ server/routes/tasks/extraTask1.js
@@ -37,7 +37,7 @@ router.post('/proxy', async (req, res) => {
     }
 
     const response = await fetch(url, fetchOptions);
-    const contentType = response.headers.get('content-type') || ' ';
+    const contentType = response.headers.get('content-type') || 'application/octet-stream';
 
     if (contentType.startsWith('image/')) {
       if (contentType === 'image/svg+xml') {
