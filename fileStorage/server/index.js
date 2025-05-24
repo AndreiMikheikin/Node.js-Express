ab92037e510a5c1f06dbd0f9e23b216d98d561d6
Andrei Mikheikin <mikhejyshka@gmail.com>
2025-05-24 17:16:15 +0300

    all interfaces

--- fileStorage/server/index.js
+++ fileStorage/server/index.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 3335;
 const UPLOAD_DIR = path.join(__dirname, '..', 'uploads');
 const META_PATH = path.join(UPLOAD_DIR, 'meta.json');
 const httpServer = http.createServer(app); // Создаём HTTP-сервер на основе Express
-const ws = new WebSocketServer({ server: httpServer }); // Привязываем WS к этому же серверу
+const wss = new WebSocketServer({ server: httpServer }); // Привязываем WS к этому же серверу
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
@@ -223,7 +223,7 @@ app.get('/files', async (req, res) => {
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-ws.on('connection', ws => {
+wss.on('connection', ws => {
   console.log('[WS] Новое соединение');
 
   ws.on('message', async msg => {
